13f04fae8468aca96d8c321bd17c7d0e82d2e2c2
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-07 12:05:47 +0200

    price definition + discount code validation

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 `Quando l’utente fa click sul bottone “send” del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
-Il prezzo orario per una commissione varia in questo modo:
-se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50€ l’ora
-se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.30€ l’ora
-se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.60€
-L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
-- YHDNU32
-- JANJC63
-- PWKCN25
-- SJDPO96
-- POCIE24
-Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. 
-Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido 
-e il prezzo finale viene calcolato senza applicare sconti. 
-Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” 
-(con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto il bottone send
+
+
 
 Bonus:
 Quando l’utente clicca sul pulsante “Send”, se il codice promozionale inserito non è valido, 
@@ -46,25 +33,45 @@ projectAnalysisServicePrice = fixedToTwoTransform(projectAnalysisServicePrice);
 
 //CREO E APPENDO ALL'ELEMENTO HTML select LA LISTA DI TIPI DI LAVORO CON IL LORO PREZZO
 //TODO: CREARE ARRAY SERVIZI, ARRAY PREZZI, CICLO FOR PER CREARE E APPENDERE ELEMENTI ALLA LISTA
+//TODO: RACCHIUDERE LA CREAZIONE DELLA LISTA IN UNA FUNZIONE
 
 let listOption1 = document.createElement("option");
+listOption1.value = ` ${backendServicePrice}`
 listOption1.innerText=`${backendService}`+ ` ${backendServicePrice} €`; 
 
 document.getElementById("work").appendChild(listOption1);
 
 let listOption2 = document.createElement("option");
+listOption2.value = ` ${frontendServicePrice}`
 listOption2.innerText=`${frontendService}`+ ` ${frontendServicePrice} €`; 
 
 document.getElementById("work").appendChild(listOption2);
 
 let listOption3 = document.createElement("option");
+listOption3.value = ` ${projectAnalysisServicePrice}`
 listOption3.innerText=`${projectAnalysisService}`+ ` ${projectAnalysisServicePrice} €`; 
 
 document.getElementById("work").appendChild(listOption3);
 
+console.log(document.getElementById("work").value);
 
+`
+Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. 
+Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido 
+e il prezzo finale viene calcolato senza applicare sconti. 
+Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” 
+(con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto il bottone send`
 
+//CREAZIONE DEL PREZZO
+let price = typeOfWork * hoursRequested * discount;
 
+//VALIDAZIONE CODICE SCONTO
+
+let userCodeInput = document.getElementById("discount").value;
+console.log(userCodeInput);
+if(discountCodesArray.includes(userCodeInput)){
+    discount = 0.75;
+} else {discount= 1;}
 
 
 
